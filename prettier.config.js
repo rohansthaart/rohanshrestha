d72db6c144269628dad19af6c091385ef504bdc0
rohansthaart <rohan-stha@live.com>
2024-01-06 22:17:49 +0545

    start: project start

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+    arrowParenns: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endoOfLie: "lf",
+    semi:false,
+    tabWidth: 2,
+    trailingComma: "none",
+
+}
+
+module.exports = options;
