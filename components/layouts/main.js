4ead05a2cc69fdff18113ef71b9ecbd37324327a
rohansthaart <rohan-stha@live.com>
2024-01-09 22:05:59 +0545

    nochange

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,37 +1,42 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import NavBar from '../navbar'
-import { Box, Container } from '@chakra-ui/react'
-import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import NavBar from '../navbar';
+import { Box, Container } from '@chakra-ui/react';
+import Footer from '../footer';
+import VoxelDogLoader from '../voxel-dog-loader';
 
 const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
   ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+  loading: () => <VoxelDogLoader />,
+});
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <meta name="description" content="Rohan's homepage" />
-            <meta name="author" content="Rohan Shrestha" />
-            <link rel="shortcut icon" href="/images/logo-dark.png" type="image/x-icon" />
-            <title>Rohan Shrestha - Homepage</title>
-            </Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Rohan's homepage" />
+        <meta name="author" content="Rohan Shrestha" />
+        <link
+          rel="shortcut icon"
+          href="/images/logo-dark.png"
+          type="image/x-icon"
+        />
+        <title>Rohan Shrestha - Homepage</title>
+      </Head>
 
-<NavBar path={router.asPath} />
+      <NavBar path={router.asPath} />
 
-<Container maxW="container.md" pt={14}>
-  <LazyVoxelDog />
+      <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
 
-  {children}
+     
+        {children}
 
-  <Footer />
-</Container>
-</Box>
-)
-}
+        <Footer />
+      </Container>
+    </Box>
+  );
+};
 
-export default Main
+export default Main;
