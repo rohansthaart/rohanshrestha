eee3fa761b29fc1909f199af296311dbbeea14f2
rohansthaart <rohan-stha@live.com>
2024-01-09 20:43:45 +0545

    fix: workindex

--- pages/works/index.js
+++ pages/works/index.js
@@ -20,7 +20,7 @@ const Works = () => {
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
             <WorkGridItem
-              id="Nagarpalika"
+              id="nagarpalika"
               title="Nagarpalika Service Delivery"
               thumbnail={ServiceDelevery}
             >
