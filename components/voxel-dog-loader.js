01ebb8372c71b648ddee83cbd5cf59f5ae2540d4
rohansthaart <rohan-stha@live.com>
2024-01-09 20:38:39 +0545

    urlDogGlb

--- components/voxel-dog-loader.js
+++ components/voxel-dog-loader.js
@@ -1,5 +1,5 @@
-import { forwardRef } from 'react';
-import { Box, Spinner } from '@chakra-ui/react';
+import { forwardRef } from 'react'
+import { Box, Spinner } from '@chakra-ui/react'
 
 export const DogSpinner = () => (
   <Spinner
@@ -10,7 +10,7 @@ export const DogSpinner = () => (
     ml="calc(0px - var(--spinner-size) / 2)"
     mt="calc(0px - var(--spinner-size))"
   />
-);
+)
 
 export const DogContainer = forwardRef(({ children }, ref) => (
   <Box
@@ -25,16 +25,17 @@ export const DogContainer = forwardRef(({ children }, ref) => (
   >
     {children}
   </Box>
-));
+))
 
-DogContainer.displayName = 'DogContainer'; // Add display name
+
+  DogContainer.displayName = 'DogContainer'
 
 const Loader = () => {
   return (
     <DogContainer>
       <DogSpinner />
     </DogContainer>
-  );
-};
+  )
+}
 
-export default Loader;
+export default Loader
